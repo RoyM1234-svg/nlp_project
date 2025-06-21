737c0f5267cb20faabf2c3c0fc38616278417df9
Roy mainfeld <71728669+RoyM1234-svg@users.noreply.github.com>
2025-06-21 19:30:57 +0300

    Create Batch logic (#11)
    Co-authored-by: Roy Mainfeld <rmainfeld@rmainfeld-mbp.corp.lightricks.com>

---
+++ datasets/detective_dataset.py
@@ -0,0 +1,29 @@
+from torch.utils.data import Dataset
+from typing import List
+from models.detective_model import DetectiveModel
+import pandas as pd
+from torch.utils.data import DataLoader
+
+class DetectiveDataset(Dataset):
+    """PyTorch Dataset for detective puzzles."""
+    
+    def __init__(self, 
+                 mystery_texts: List[str], 
+                 suspects_lists: List[List[str]],
+                 true_labels: List[str]):
+        self.mystery_texts = mystery_texts
+        self.suspects_lists = suspects_lists
+        self.true_labels = true_labels
+        
+    def __len__(self):
+        return len(self.mystery_texts)
+    
+    def __getitem__(self, idx):
+        return {
+            'mystery_text': self.mystery_texts[idx],
+            'suspects': self.suspects_lists[idx],
+            'true_label': self.true_labels[idx],
+            'index': idx
+        }
+    
+
