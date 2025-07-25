fce18d9cf9c12471daac06f40197ac73092daf02
leinkarin <121857473+leinkarin@users.noreply.github.com>
2025-07-25 17:10:36 +0300

    add custom collate

--- data_loaders/verifier_data_loader.py
+++ data_loaders/verifier_data_loader.py
@@ -3,10 +3,27 @@ import pandas as pd
 from musr.train_verifier import VERIFIER_QUESTION
 from custom_datasets.verifier_dataset import VerifierDataset
 
+def custom_collate_fn(batch):
+    case_names = [item['case_names'] for item in batch]
+    mystery_texts = [item['mystery_texts'] for item in batch]
+    suspects_lists = [item['suspects_lists'] for item in batch]
+    true_labels = [item['true_labels'] for item in batch]
+    generated_cots = [item['generated_cots'] for item in batch]
+    texts = [item['text'] for item in batch]
+    
+    return {
+        'case_names': case_names,
+        'mystery_texts': mystery_texts,
+        'suspects_lists': suspects_lists,
+        'true_labels': true_labels,
+        'generated_cots': generated_cots,
+        'text': texts,
+    }
+
 class VerifierDataLoader(DataLoader):
     def __init__(self, df: pd.DataFrame, batch_size: int = 1, shuffle: bool = False):
         dataset = self.create_dataset(df)
-        super().__init__(dataset, batch_size=batch_size, shuffle=shuffle)
+        super().__init__(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=custom_collate_fn)
 
     @staticmethod
     def create_dataset(df: pd.DataFrame):
